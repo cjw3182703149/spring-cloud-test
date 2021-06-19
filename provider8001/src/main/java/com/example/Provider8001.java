9c4c9bfc83351fda739ba05201928f1f6fd4e7a8
cjw3182703149 <575827443@qq.com>
2021-06-19 21:45:11 +0800

    跨域

--- provider8001/src/main/java/com/example/Provider8001.java
+++ provider8001/src/main/java/com/example/Provider8001.java
@@ -3,9 +3,13 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@MapperScan(value = "com.example.dao")
+@EnableEurekaClient // 配置eureka注解扫描
+@EnableDiscoveryClient  // 服务发现 团队合作使用，可以不写
 public class Provider8001 {
     public static void main(String[] args) {
         SpringApplication.run(Provider8001.class, args);
--- provider8001/src/main/java/com/example/controller/DeptController.java
+++ provider8001/src/main/java/com/example/controller/DeptController.java
@@ -3,16 +3,23 @@ package com.example.controller;
 import com.example.pojo.Dept;
 import com.example.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient; // 注意一下是这个包下的
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class DeptController {
 
     @Autowired
     private DeptService deptService;
+
+    // 获取一些配置的信息，就是具体的微服务
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/dept/add")
     @Transactional(rollbackFor = Exception.class)
@@ -28,5 +35,24 @@ public class DeptController {
     @GetMapping("/dept/queryall")
     public List<Dept> queryAll() {
         return deptService.queryAll();
+    }
+
+    // 注册进来的微服务信息，团队合作使用，可以不使用这个
+    @GetMapping("/dept/discovery")
+    public Object discovery() {
+        // 获取微服务列表的清单
+        List<String> services = client.getServices();
+        System.out.println("discovery=>"+services);
+        //得到具体的微服务信息,通过具体的微服务id，applicationName
+        List<ServiceInstance> provider = client.getInstances("PROVIDER");
+        for (ServiceInstance serviceInstance : provider) {
+            System.out.println(
+                    serviceInstance.getHost() + "\t" +
+                            serviceInstance.getPort()+"\t"+
+                            serviceInstance.getUri() + "\t"+
+                            serviceInstance.getServiceId()
+            );
+        }
+        return this.client;
     }
 }
