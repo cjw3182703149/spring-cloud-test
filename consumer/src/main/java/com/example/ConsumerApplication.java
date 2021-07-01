eed6b005c59bd2f66fc01374906491544d8391e0
cjw3182703149 <575827443@qq.com>
2021-07-01 23:00:38 +0800

    ribbon

--- consumer/src/main/java/com/example/ConsumerApplication.java
+++ consumer/src/main/java/com/example/ConsumerApplication.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import com.myrule.CRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient // 客户端
+@RibbonClient(name = "PROVIDER", configuration = CRule.class)//启动加载自定义的Ribbon类
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
